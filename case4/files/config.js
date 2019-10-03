12e7d2179194895283db642e5375eaef24ede9d6
webzhouting <736919203@qq.com>
2019-10-03 10:26:50 +0800

    base

---
+++ case4/files/config.js
@@ -0,0 +1,22 @@
+var datas = [
+    {
+        name:'ck-5545',
+        png:'ck-5545.png',
+        val:30
+    },
+    {
+        name:'eg6105',
+        png:'eg6105.png',
+        val:30
+    },
+    {
+        name:'egc815',
+        png:'egc815.png',
+        val:40
+    }
+]
+var gailv = [
+    {
+
+    }
+]
